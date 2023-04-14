96c3d692d4cfeb071558a34027f22232c939ba09
Daniel Souza <danielsouza51764@gmail.com>
2023-04-14 12:56:26 -0300

    test register e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,26 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'ong-name',
+      email: 'ongname@example.com',
+      phone: '9999999',
+      city: 'city',
+      uf: 'uf',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})

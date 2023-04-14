f170f3d937e69454e83c9d2d6802c91bc108b395
Daniel Souza <danielsouza51764@gmail.com>
2023-04-14 10:54:13 -0300

    jwt auth

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,13 +4,13 @@ import { authenticate } from './controllers/authenticate'
 import { caseController } from './controllers/case'
 import { getCases } from './controllers/get-cases'
 import { deleteCase } from './controllers/delete-case'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
 
-  app.post('/cases', caseController)
+  app.post('/cases', { onRequest: [verifyJwt] }, caseController)
   app.get('/cases/:userId', getCases)
   app.delete('/cases/:caseId', deleteCase)
 }
